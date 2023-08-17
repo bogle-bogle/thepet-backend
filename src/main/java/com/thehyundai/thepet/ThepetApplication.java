1947e54c68e12cb90e82bb11373206dd68be770d
BoSeon Park <bedurgi999@gmail.com>
2023-08-17 10:01:10 +0900

    initial commit

---
+++ src/main/java/com/thehyundai/thepet/ThepetApplication.java
@@ -0,0 +1,13 @@
+package com.thehyundai.thepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThepetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThepetApplication.class, args);
+	}
+
+}
