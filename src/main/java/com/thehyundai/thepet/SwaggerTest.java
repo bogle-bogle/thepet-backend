745b8d7d64bda9c6ebeb8455699529a9f8fabf77
seolhi <seolhi1212@gmail.com>
2023-08-17 14:25:26 +0900

    setting/swagger/완료

---
+++ src/main/java/com/thehyundai/thepet/SwaggerTest.java
@@ -0,0 +1,15 @@
+package com.thehyundai.thepet;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SwaggerTest {
+
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
