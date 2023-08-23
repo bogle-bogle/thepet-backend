26234eba80fabe4e2d096536eb420aac7a750080
송민진 <m_6595@naver.com>
2023-08-23 11:33:53 +0900

    fix(setting): @Service 위치 변경

--- src/main/java/com/thehyundai/thepet/cart/CartServiceImpl.java
+++ src/main/java/com/thehyundai/thepet/cart/CartServiceImpl.java
@@ -2,8 +2,10 @@ package com.thehyundai.thepet.cart;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
 @Log4j2
+@Service
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
     private final CartMapper cartMapper;
