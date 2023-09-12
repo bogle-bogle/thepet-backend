7afb5fed0b987d661bbf77410e2fd20cfd32dbee
송민진 <m_6595@naver.com>
2023-09-12 18:22:11 +0900

    fix: 배포 오류 수정

--- src/main/java/com/thehyundai/thepet/global/config/RedisCacheConfig.java
+++ src/main/java/com/thehyundai/thepet/global/config/RedisCacheConfig.java
@@ -1,7 +1,5 @@
 package com.thehyundai.thepet.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
