4fa4b35a9003a1ee0902f824b8978257f7d04912
송민진 <m_6595@naver.com>
2023-09-16 18:25:16 +0900

    fix: swagger 정상화

--- src/main/java/com/thehyundai/thepet/ThepetApplication.java
+++ src/main/java/com/thehyundai/thepet/ThepetApplication.java
@@ -1,10 +1,12 @@
 package com.thehyundai.thepet;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @EnableCaching
 public class ThepetApplication {
