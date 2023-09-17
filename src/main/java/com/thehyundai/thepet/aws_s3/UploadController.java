d61fafe1362a0beff906808615aa686adc7e0efe
송민진 <m_6595@naver.com>
2023-09-17 21:45:11 +0900

    feat(admin): S3에 사진 업로드하는 기능 구현
    #137

--- src/main/java/com/thehyundai/thepet/aws_s3/UploadController.java
+++ src/main/java/com/thehyundai/thepet/aws_s3/UploadController.java
@@ -22,19 +22,36 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class UploadController {
     private final S3Service s3Service;
+    private final String S3_PET_FEED_BUCKET = "heendy-feed";
+    private final String S3_ADMIN_IMG_SRC_BUCKET = "heendy-assets";
 
     @PostMapping
-    // @PreAuthorize("hasRole('ROLE_USER')") => 사용자 제한 시큐리티한다면..
+    // @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         try {
             // 파일의 MIME 유형을 확인하여 이미지 파일인지 확인 => image/jpeg, image/png, image/gif에 한함
             if (!file.getContentType().startsWith("image/")) {
                 return new ResponseEntity<>("이미지 파일만 업로드 가능", HttpStatus.BAD_REQUEST);
             }
-            String imageUrl = s3Service.uploadFileAndGetUrl(file);
+            String imageUrl = s3Service.uploadFileAndGetUrl(file, S3_PET_FEED_BUCKET);
             return new ResponseEntity<>(imageUrl, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>("이미지 업로드 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("이미지 업로드 실패 : " + e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/admin")
+    // @PreAuthorize("hasRole('ROLE_ADMIN')") => 사용자 제한 시큐리티한다면..
+    public ResponseEntity<String> uploadAdminFile(@RequestParam("file") MultipartFile file) throws IOException {
+        try {
+            // 파일의 MIME 유형을 확인하여 이미지 파일인지 확인 => image/jpeg, image/png, image/gif에 한함
+            if (!file.getContentType().startsWith("image/")) {
+                return new ResponseEntity<>("이미지 파일만 업로드 가능", HttpStatus.BAD_REQUEST);
+            }
+            String imageUrl = s3Service.uploadFileAndGetUrl(file, S3_ADMIN_IMG_SRC_BUCKET);
+            return new ResponseEntity<>(imageUrl, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("이미지 업로드 실패 : " + e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
