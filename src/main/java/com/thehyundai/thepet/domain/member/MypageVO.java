485504c5ec0120fe4dac9b2f70ea25f8f4ea4572
송민진 <m_6595@naver.com>
2023-09-20 11:30:24 +0900

    ci: 빌드 관련 에러 수정

--- src/main/java/com/thehyundai/thepet/domain/member/MypageVO.java
+++ src/main/java/com/thehyundai/thepet/domain/member/MypageVO.java
@@ -5,18 +5,15 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class MypageVO {
     private Integer myPetCnt;
-    private Integer onDeliveryCnt = 0;
+    private final Integer onDeliveryCnt = 0;
     private Integer subscriptionCnt;
-    private Integer couponCnt = 3;
+    private final Integer couponCnt = 3;
 
     public MypageVO(Integer myPetCnt, Integer subscriptionCnt) {
         this.myPetCnt = myPetCnt;
-        this.onDeliveryCnt = 0;
         this.subscriptionCnt = subscriptionCnt;
-        this.couponCnt = 3;
     }
 }
