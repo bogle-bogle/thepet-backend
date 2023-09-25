b41f5a6ab4d71250dd4031f677271d5475f91f11
송민진 <m_6595@naver.com>
2023-09-25 10:26:12 +0900

    feat(kafka): producer 여러 개 처리 완료
    #188

--- src/main/java/com/thehyundai/thepet/global/kafka/ServiceLogProducer.java
+++ src/main/java/com/thehyundai/thepet/global/kafka/ServiceLogProducer.java
@@ -3,21 +3,22 @@ package com.thehyundai.thepet.global.kafka;
 import com.thehyundai.thepet.global.timetrace.AopServiceVO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
 public class ServiceLogProducer {
-    private static final String TOPIC = "service_execution_time_log";
+    private static final String SERVICE_LOG_TOPIC = "service_execution_time_log";
     private final KafkaTemplate<String, AopServiceVO> kafkaTemplate;
 
     @Autowired
-    public ServiceLogProducer(KafkaTemplate kafkaTemplate) {
+    public ServiceLogProducer(@Qualifier("serviceLogKafkaTemplate")KafkaTemplate kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendMessage(AopServiceVO message) {
-        this.kafkaTemplate.send(TOPIC, message);
+        this.kafkaTemplate.send(SERVICE_LOG_TOPIC, message);
     }
 }
