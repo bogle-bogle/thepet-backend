03134166808c4193ef4ccc3a2389f51e6843ab58
seolhi <seolhi1212@gmail.com>
2023-09-25 17:38:15 +0900

    feat(ordercomplete->sms) : 결제 완료 문자 전송

--- src/main/java/com/thehyundai/thepet/external/sms/PaymentSmsEvent.java
+++ src/main/java/com/thehyundai/thepet/external/sms/PaymentSmsEvent.java
@@ -1,9 +1,19 @@
-package com.thehyundai.thepet.global.sms;
+package com.thehyundai.thepet.external.sms;
 
+import com.thehyundai.thepet.domain.order.OrderVO;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.ApplicationEvent;
 
 @Getter
 @Log4j2
-public class PaymentSmsEvent {
+public class PaymentSmsEvent extends ApplicationEvent {
+    private OrderVO order;
+
+
+    public PaymentSmsEvent(Object source, OrderVO order) {
+        super(source);
+        this.order = order;
+        log.info("결제 SMS 이벤트 실행");
+    }
 }
