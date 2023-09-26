4b21be84942d764abc3227472bec0ddcf0e31963
송민진 <m_6595@naver.com>
2023-09-26 22:45:20 +0900

    feat: 강아지 등록 로직 spring으로 이관
    #188

--- src/main/java/com/thehyundai/thepet/domain/mypet/pet/PetService.java
+++ src/main/java/com/thehyundai/thepet/domain/mypet/pet/PetService.java
@@ -1,13 +1,14 @@
 package com.thehyundai.thepet.domain.mypet.pet;
 
 
+import com.thehyundai.thepet.domain.mypet.club.ClubRequestVO;
 import com.thehyundai.thepet.external.ocrnlp.OcrNlpResultVO;
 
 import java.util.List;
 
 public interface PetService {
 
-    String registerClub(String token,PetVO petVO);
+    PetVO registerClub(String token, ClubRequestVO requestVO);
 
     OcrNlpResultVO updateFeed(PetSuggestionRequestVO requestVO);
 
--- src/main/java/com/thehyundai/thepet/global/config/WebConfig.java
+++ src/main/java/com/thehyundai/thepet/global/config/WebConfig.java
@@ -38,6 +38,8 @@ public class WebConfig implements WebMvcConfigurer {
                                      "/api/backoffice/**",
                                      "/api/review/**",
                                      "/api/hc/branch",
+                                     "/api/event",
+                                     "/api/pet",
                                      "/api/hc/branch/{branchCode}/reservation",
                                      "/api/hc/updateStatus/{productId}/{type}/{newValue}",
                                      "/docs", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**");
