c46cc9c3814952e3359c0f751dfad96f184044b3
seolhi <seolhi1212@gmail.com>
2023-09-28 19:55:52 +0900

    feat 중간커밋

--- src/main/java/com/thehyundai/thepet/external/tosspayment/PaymentReqDTO.java
+++ src/main/java/com/thehyundai/thepet/external/tosspayment/PaymentReqDTO.java
@@ -11,8 +11,13 @@ import lombok.*;
 // 결제 승인 요청 DTO
 public class PaymentReqDTO {
 
+    // 일반결제
     private Integer amount;
     private String orderId;
     private String paymentKey;
+
+    // 정기결제
+    private String customerKey;
+    private String orderName;
 
 }
